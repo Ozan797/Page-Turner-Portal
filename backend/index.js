d61973d8fe79e488b35ab46d621f5ba67bdde1d4
Ozan Gokberk <ozan8@hotmail.co.uk>
2024-01-03 13:18:12 +0000

    Can retrieve details in frontend

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import mysql from "mysql2";
-
+import cors from "cors";
 const app = express();
 const port = 8800;
 const serverUrl = `http://localhost:${port}`;
@@ -11,27 +11,35 @@ const db = mysql.createConnection({
   database: "bookshelf",
 });
 app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.json("hello this is the backend");
-});
+app.use(cors());
 
 app.get("/books", (req, res) => {
   const q = "SELECT * FROM books";
   db.query(q, (err, data) => {
-    if (err) return res.json(err);
+    if (err) {
+      console.log(err);
+      return res.json(err);
+    }
     return res.json(data);
   });
 });
 
 app.post("/books", (req, res) => {
-  const q = "INSERT INTO books (`title`,`desc`,`cover`) VALUES (?)";
-  const values = [req.body.title, req.body.desc, req.body.cover];
+  const q = "INSERT INTO books(`title`, `desc`, `price`, `cover`) VALUES (?)";
+
+  const values = [
+    req.body.title,
+    req.body.desc,
+    req.body.price,
+    req.body.cover,
+  ];
+
   db.query(q, [values], (err, data) => {
-    if (err) return res.json(err);
+    if (err) return res.send(err);
     return res.json(data);
   });
 });
+
 
 app.listen(port, () => {
   console.log(`Server running at ${serverUrl}`);
---
+++ frontend/src/pages/Books.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+const Books = () => {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    const fetchAllBooks = async () => {
+      try {
+        const res = await axios.get("http://localhost:8800/books");
+        setBooks(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchAllBooks();
+  }, []);
+
+  return (
+    <div>
+      <h1>Lama Book Shop</h1>
+      <div className="books">
+        {books.map((book) => (
+          <div key={book.id} className="book">
+            <img src={book.cover} alt="" />
+            <h2>{book.title}</h2>
+            <p>{book.desc}</p>
+            <span>${book.price}</span>
+          </div>
+        ))}
+      </div>
+      <button><Link to="/add">Add new book</Link></button>
+    </div>
+  );
+};
+export default Books;
